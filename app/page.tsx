0760523fea64782055d6e178232471fbb4ca8bd0
smarttechfire <educationfire552@gmail.com>
2024-07-11 23:54:35 +0530

    Home Basic user data

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import { currentUser } from "@clerk/nextjs/server";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const user = await currentUser();
+  console.log(user);
   return (
-    <div className=" absolute top-1/3 left-1/2 -translate-x-1/2 -translate-y-1/2">
-      <div className=" flex flex-col items-center text-center">Home</div>
+    <div className=" absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <div className=" flex flex-col items-center text-center">{user?.username}</div>
     </div>
   );
 }
