6188f84113019131e76dbfad882c1b81703b1dde
smarttechfire <educationfire552@gmail.com>
2024-07-11 23:56:48 +0530

    remove log

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default async function Home() {
   const user = await currentUser();
-  console.log(user);
+  // console.log(user);
   return (
     <div className=" absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
       <div className=" flex flex-col items-center text-center">{user?.username}</div>
